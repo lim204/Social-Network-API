2078de466900be1e20bf2e3035fa6d0c0749588f
Laura M <laurita_im@hotmail.com>
2023-06-25 17:39:38 -0700

    setting reaction file model

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,51 +1,36 @@
 const { Schema, model } = require('mongoose');
 
-// Schema to create User model
-const userSchema = new Schema(
-    {
-      username: {
-       type: String,
-      unique: true,
-      require: true,
-      trim: true
-      },
-       email:
-      { type: String,
-        require: true,
-        unique: true,
-        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please use a valid email address']
 
-      },
-      thoughts:[
-        {
-          type: Schema.Types.ObjectId,
-          ref: 'Thought',
+// Schema to create User model
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => Types.ObjectId(),
         },
-      ],
-      friends:[
-        {
-          type: Schema.Types.ObjectId,
-          ref: 'User',
+        reactionBody: {
+            type: String,
+            require: true,
+            maxLenght: 280,
         },
-      ],
+        username:
+        {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
     },
     {
-      // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-      // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
-      toJSON: {
-        virtuals: true,
-      },
-      id: false,
-    }
-  );
-  
-  // Create a virtual property `fullName` that gets and sets the user's full name
-  userSchema
-    .virtual('friendCount')
-    .get(function () {
-      return this.friends.length;
-    })
+        // use a fetter method of format the timestamp 
     
-const User = model ('user',userSchema);    
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
 
-module.exports = User;
+module.exports = reactionSchema;
